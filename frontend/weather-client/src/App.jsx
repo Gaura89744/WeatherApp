b3283aedc782c75a5aa9569a8e1e1df79224cd46
Gaura89744 <gauravsrivastavass007@gmail.com>
2025-07-31 17:18:36 +0530

    update vite.config.js

--- frontend/weather-client/src/App.jsx
+++ frontend/weather-client/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
 
   const baseURL = import.meta.env.VITE_API_BASE_URL;
 
+  const cityURL = import.meta.env.CITY_API_BASE_URL;
 
   const getWeatherIcon = (weatherMain) => {
     switch (weatherMain.toLowerCase()) {
@@ -28,9 +29,7 @@ function App() {
     if (!query) return;
 
     try {
-      const res = await fetch(
-        `http://localhost:5000/api/city-suggestions?name=${query}`
-      );
+      const res = await fetch(`${cityURL}/api/city-suggestions?name=${query}`);
 
       if (!res.ok) {
         throw new Error(`Server error: ${res.status}`);
